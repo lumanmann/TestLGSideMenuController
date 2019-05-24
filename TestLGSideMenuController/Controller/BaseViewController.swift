86f2c3da1353440d0fe9735e5eab6625cf44febb
Natalie <nwy0206@gmail.com>
2019-05-24 19:11:55 +0800

    Add asset

---
+++ TestLGSideMenuController/Controller/BaseViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BaseViewController.swift
+//  TestLGSideMenuController
+//
+//  Created by Natalie Ng on 2019/5/24.
+//  Copyright © 2019 appi. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TestLGSideMenuController/View/BaseButton.swift
@@ -0,0 +1,21 @@
+//
+//  BaseButton.swift
+//  TestLGSideMenuController
+//
+//  Created by Natalie Ng on 2019/5/24.
+//  Copyright © 2019 appi. All rights reserved.
+//
+
+import UIKit
+
+class BaseButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
